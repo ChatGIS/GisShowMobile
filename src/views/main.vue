efe980c5792ff087c37c0bcde9dd8b619291718a
Dreamice <dreamice13@foxmail.com>
2023-01-31 21:42:40 +0800

    leaflet展示地图

--- src/views/main.vue
+++ src/views/main.vue
@@ -1,14 +1,29 @@
 <script setup lang='ts'>
+import { onMounted } from 'vue'
+import * as L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+onMounted(() => {
+    initMap()
+})
+// 地图初始化
+const initMap = () => {
+    const map = L.map('lMap').setView([51.505, -0.09], 13)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map)
+}
 
 </script>
 
 <template>
-    <div>
-        <h1>GSM</h1>
-        <van-button type="primary">主要按钮</van-button>
+    <div id="lMap">
     </div>
 </template>
 
 <style scoped>
-
+#lMap {
+    height: 100%;
+}
 </style>
