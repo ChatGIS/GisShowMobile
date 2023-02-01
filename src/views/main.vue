3a8f77ce5a6e286ff30c33c13cf18df1215d8365
Dreamice <dreamice13@foxmail.com>
2023-02-01 18:15:49 +0800

    添加marker

--- src/views/main.vue
+++ src/views/main.vue
@@ -13,6 +13,7 @@ const initMap = () => {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map)
+    var marker = L.marker([51.5, -0.09]).addTo(map)
 }
 
 </script>
